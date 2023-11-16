053f56fb8c1f12bf77decbb899e94981b0a63ff1
Alisa03 <88279100+Alisa03@users.noreply.github.com>
2023-11-16 22:30:03 +0300

    fix scroll

--- src/components/Scrolls/InfiniteLoopSlider.tsx
+++ src/components/Scrolls/InfiniteLoopSlider.tsx
@@ -1,11 +1,12 @@
 import s from './scrolls.module.scss'
 
 const InfiniteLoopSlider = ({ children, duration, reverse = false }: any) => {
+    const style = {
+        "--duration": `${duration}ms`,
+        '--direction': reverse ? 'reverse' : 'normal'
+    } as React.CSSProperties;
     return (
-        <div style={{
-            "--duration": `${duration}ms`,
-            '--direction': reverse ? 'reverse' : 'normal'
-        }}>
+        <div style={style}>
             <div className={s.inner}>
                 {children}
                 {children}
